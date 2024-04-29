087c2ae5cc7a3a91ae89429104b03234fd382e2a
jgonzalezef <jesus.gonzalez@enviaflores.com>
2024-04-29 13:48:05 -0600

    welcome to estore api

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express = require('express');
 
 app = express()
 
+app.use("/",(req,res) => {
+    res.json({
+        "message":"Welcome to Estore API"
+    });
+});
+
 app.listen(3000,() =>{
     console.log("Servidor corriendo en el puerto 3000");
 });
